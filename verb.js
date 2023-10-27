7c5c6dc94540fea441a6a0146361ba18f170ecc9
skks1991 <basher.send@gmail.com>
2023-10-27 11:03:36 +0600

    updated

--- verb.js
+++ verb.js
@@ -1,41 +1,41 @@
 function r1(){
-    document.getElementById("demo1").innerHTML = "g";
+    document.getElementById("demo1").innerHTML = "is";
     
   }
   function r2(){
-    document.getElementById("demo2").innerHTML = "b";
+    document.getElementById("demo2").innerHTML = "refers";
     
   }
   function r3(){
-    document.getElementById("demo3").innerHTML = "f";
+    document.getElementById("demo3").innerHTML = "make";
     
   }
   function r4(){
-    document.getElementById("demo4").innerHTML = "d";
+    document.getElementById("demo4").innerHTML = "devote";
     
   }
   function r5(){
-    document.getElementById("demo5").innerHTML = "a";
+    document.getElementById("demo5").innerHTML = "are";
     
   }
   function r6(){
-    document.getElementById("demo6").innerHTML = "h";
+    document.getElementById("demo6").innerHTML = "are";
     
   }
   function r7(){
-    document.getElementById("demo7").innerHTML = "c";
+    document.getElementById("demo7").innerHTML = "have";
     
   }
   function r8(){
-    document.getElementById("demo8").innerHTML = "e";
+    document.getElementById("demo8").innerHTML = "be marked";
     
   }
   function r9(){
-    document.getElementById("demo8").innerHTML = "k";
+    document.getElementById("demo9").innerHTML = "hesitates";
     
   }
   function r10(){
-    document.getElementById("demo8").innerHTML = "l";
+    document.getElementById("demo10").innerHTML = "dedicate";
     
   }
   
